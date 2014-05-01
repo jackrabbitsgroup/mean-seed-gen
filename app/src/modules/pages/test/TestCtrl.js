f13069ab3f812d64a2373cdb28eea910f6ec850f
Luke Madera <luke.madera@gmail.com>
2014-04-30 20:17:40 -0700

    chore: remove old files

--- app/src/modules/pages/test/TestCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('myApp').controller('TestCtrl', ['$scope', '$timeout', 'appHttp', 'UserModel', '$location', function($scope, $timeout, appHttp, UserModel, $location) {
-	
-	$scope.user =UserModel.load();
-}]);
