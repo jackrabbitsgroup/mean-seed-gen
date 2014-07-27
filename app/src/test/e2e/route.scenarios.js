a393d01c20243cb3c9be6c7fcd2c7f99cc01aed8
Luke Madera <luke.madera@gmail.com>
2014-07-27 08:43:42 -0700

    '2014-07-27 08:43:39 yo mean-seed update'

--- app/src/test/e2e/route.scenarios.js
+++ app/src/test/e2e/route.scenarios.js
@@ -12,9 +12,9 @@ function hasClass(class1, params) {
 
 describe("E2E: Testing Routes", function() {
 
-	var ptor =protractor.getInstance();
+	// var ptor =protractor.getInstance();
 	
-	// ptor.sleep(2000);		//trying to get PhantomJS browser to work.. Angular is not defined/ready yet?..
+	// browser.sleep(2000);		//trying to get PhantomJS browser to work.. Angular is not defined/ready yet?..
 	
 	//create random id so this user is unique
 	var randString =Math.random().toString(36).substring(7)
@@ -35,85 +35,85 @@ describe("E2E: Testing Routes", function() {
 	
 	beforeEach(function() {
 		// ptor =protractor.getInstance();
-		// ptor.get('/');
-		// ptor.driver.get('#/');
-		// ptor.waitForAngular();		//trying to get PhantomJS browser to work.. Angular is not defined/ready yet?..
+		// browser.get('/');
+		// browser.driver.get('#/');
+		// browser.waitForAngular();		//trying to get PhantomJS browser to work.. Angular is not defined/ready yet?..
 	});
 
 	it('should jump to the default page path when / is accessed', function() {
-		ptor.get('/');
-		expect(ptor.getCurrentUrl()).toContain('/'+defaultPage);
+		browser.get('/');
+		expect(browser.getCurrentUrl()).toContain('/'+defaultPage);
 	});
 	
 	//no login required routes
 	it('should have a working /password-reset route', function() {
-		ptor.get('/password-reset');
-		expect(ptor.getCurrentUrl()).toContain("/password-reset");
+		browser.get('/password-reset');
+		expect(browser.getCurrentUrl()).toContain("/password-reset");
 	});
 	
 	//sign up
 	it('should have a working /login route', function() {
-		// ptor.get('/logout');		//ensure user is logged out first (otherwise will redirect to main/logged in page)
-		// ptor.get('/user-deactivate?email='+user1.email);		//ensure user is not already a member (otherwise won't be able to sign up (again))		//UPDATE - now cleaning up at end with user-delete so no longer need this (and it slows things down anyway)
+		// browser.get('/logout');		//ensure user is logged out first (otherwise will redirect to main/logged in page)
+		// browser.get('/user-deactivate?email='+user1.email);		//ensure user is not already a member (otherwise won't be able to sign up (again))		//UPDATE - now cleaning up at end with user-delete so no longer need this (and it slows things down anyway)
 		
-		ptor.get('/login');
-		expect(ptor.getCurrentUrl()).toContain("/login");
+		browser.get('/login');
+		expect(browser.getCurrentUrl()).toContain("/login");
 		
-		// var btnLogin =ptor.findElement(protractor.By.css('.login-form .btn-primary:submit'));
-		var btnLogin =ptor.findElement(protractor.By.css('.login-form .btn-primary'));
+		// var btnLogin =browser.findElement(by.css('.login-form .btn-primary:submit'));
+		var btnLogin =browser.findElement(by.css('.login-form .btn-primary'));
 		expect(btnLogin.getText()).toContain('Login');
 	});
 	
 	it('should have a working /signup route and allow creating a new user', function() {
-		ptor.get('/signup');
-		expect(ptor.getCurrentUrl()).toContain("/signup");
+		browser.get('/signup');
+		expect(browser.getCurrentUrl()).toContain("/signup");
 		
 		/*
 		//outdated now that have both login and signup pages separately
 		
 		//click button to go to sign up
-		// var ele =ptor.findElement(protractor.By.css('.login-signup .btn-link:nth-child(1)'));		//doesn't work - eq() is different than css selectors and handles this?
-		// var ele =ptor.findElement(protractor.By.css('.login-signup .btn-link'));
-		// var ele =ptor.findElement(protractor.By.css('.login-signup div:nth-child(1) div:nth-child(2) .btn-link'));		//doesn't work - wtf??
-		// var ele =ptor.findElement(protractor.By.css('.login-signup > div:nth-child(1) > div:nth-last-child(1) .btn-link'));		//works... apparently only (1) works for nested nth-child or nth-last-child?? wtf?
-		// var ele =ptor.findElement(protractor.By.xpath("/div[@class='login-signup']/div[1]/div[2]/*[@class='btn-link']"));
-		// var ele =ptor.findElement(protractor.By.xpath("//div[@class='login-signup']/div[1]/div[2]"));
-		// var ele =ptor.findElement(protractor.By.xpath("/html/body/div/div/div/div/div[1]/div[2]/span[@class='btn-link']"));
-		// var ele =ptor.findElement(protractor.By.xpath("/html/body/div/div/div/div[contains(concat(' ', @class, ' '), ' login-signup ')]/div[1]/div[2]/*[@class='btn-link']"));
-		// var ele =ptor.findElement(protractor.By.xpath("//div[contains(concat(' ', @class, ' '), ' login-signup ')]/div[1]/div[2]/*[@class='btn-link']"));
-		var ele =ptor.findElement(protractor.By.xpath("//div["+hasClass('login-signup')+"]/div[1]/div[2]/*[@class='btn-link']"));
+		// var ele =browser.findElement(by.css('.login-signup .btn-link:nth-child(1)'));		//doesn't work - eq() is different than css selectors and handles this?
+		// var ele =browser.findElement(by.css('.login-signup .btn-link'));
+		// var ele =browser.findElement(by.css('.login-signup div:nth-child(1) div:nth-child(2) .btn-link'));		//doesn't work - wtf??
+		// var ele =browser.findElement(by.css('.login-signup > div:nth-child(1) > div:nth-last-child(1) .btn-link'));		//works... apparently only (1) works for nested nth-child or nth-last-child?? wtf?
+		// var ele =browser.findElement(by.xpath("/div[@class='login-signup']/div[1]/div[2]/*[@class='btn-link']"));
+		// var ele =browser.findElement(by.xpath("//div[@class='login-signup']/div[1]/div[2]"));
+		// var ele =browser.findElement(by.xpath("/html/body/div/div/div/div/div[1]/div[2]/span[@class='btn-link']"));
+		// var ele =browser.findElement(by.xpath("/html/body/div/div/div/div[contains(concat(' ', @class, ' '), ' login-signup ')]/div[1]/div[2]/*[@class='btn-link']"));
+		// var ele =browser.findElement(by.xpath("//div[contains(concat(' ', @class, ' '), ' login-signup ')]/div[1]/div[2]/*[@class='btn-link']"));
+		var ele =browser.findElement(by.xpath("//div["+hasClass('login-signup')+"]/div[1]/div[2]/*[@class='btn-link']"));
 		expect(ele.getText()).toContain('Sign Up');
 		ele.click();
 		*/
 		
 		//fill in signup form
 		var formSelector ='.signup-form form';
-		ptor.findElement(protractor.By.css(formSelector+' .jrg-forminput-cont:nth-child(1) input')).sendKeys(user1.name);
-		ptor.findElement(protractor.By.css(formSelector+' .jrg-forminput-cont:nth-child(2) input')).sendKeys(user1.email);
-		// ptor.findElement(protractor.By.css(formSelector+' .jrg-forminput-cont:nth-child(3) input')).sendKeys(user1.phone.number);
-		ptor.findElement(protractor.By.css(formSelector+' .jrg-forminput-cont:nth-child(3) input')).sendKeys(user1.password);
-		ptor.findElement(protractor.By.css(formSelector+' .jrg-forminput-cont:nth-child(4) input')).sendKeys(user1.password);
+		browser.findElement(by.css(formSelector+' .jrg-forminput-cont:nth-child(1) input')).sendKeys(user1.name);
+		browser.findElement(by.css(formSelector+' .jrg-forminput-cont:nth-child(2) input')).sendKeys(user1.email);
+		// browser.findElement(by.css(formSelector+' .jrg-forminput-cont:nth-child(3) input')).sendKeys(user1.phone.number);
+		browser.findElement(by.css(formSelector+' .jrg-forminput-cont:nth-child(3) input')).sendKeys(user1.password);
+		browser.findElement(by.css(formSelector+' .jrg-forminput-cont:nth-child(4) input')).sendKeys(user1.password);
 		
 		//submit form
-		ptor.findElement(protractor.By.css('.signup-form form button.btn-primary')).click();
+		browser.findElement(by.css('.signup-form form button.btn-primary')).click();
 		
 		//expect page redirect after successful login
-		expect(ptor.getCurrentUrl()).toContain("/"+defaultPage);
+		expect(browser.getCurrentUrl()).toContain("/"+defaultPage);
 	});
 	
 	//clean up - remove user (this is VITAL in case running tests in parallel - need to clear out afterward; deactivating at top isn't good enough; they should be gone all together)
 	//must be LAST since other routes depend on being logged in!
 	it('clean up - should delete user via a working /user-delete route', function() {
-		ptor.get('/user-delete');
-		expect(ptor.getCurrentUrl()).toContain("/"+defaultPage);		//should go to default page after logout is complete
+		browser.get('/user-delete');
+		expect(browser.getCurrentUrl()).toContain("/"+defaultPage);		//should go to default page after logout is complete
 	});
 	
 	/*
 	//now deleting user, which will call logout automatically
 	it('should have a working /logout route', function() {
-		ptor.get('/logout');
-		// expect(ptor.getCurrentUrl()).toContain("/logout");
-		expect(ptor.getCurrentUrl()).toContain("/login");		//should go to login after logout is complete
+		browser.get('/logout');
+		// expect(browser.getCurrentUrl()).toContain("/logout");
+		expect(browser.getCurrentUrl()).toContain("/login");		//should go to login after logout is complete
 	});
 	*/
 
